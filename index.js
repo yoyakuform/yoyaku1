6a14d56613661afb69e1d5a13b797de210b53e25
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-22 13:21:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,11 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var date01 = $('input[name="date01"]').val();
+        var time01 = $('input[name="time01"]').val();
         var date02 = $('input[name="date02"]').val();
+        var time02 = $('input[name="time02"]').val();
         var date03 = $('input[name="date03"]').val();
+        var time03 = $('input[name="time03"]').val();
         var number = $('input[name="number"]:checked').val();
         var names = '';
         $('#form-name').children().each(function (i, elm) {
@@ -45,7 +48,7 @@ $(function () {
         var tel = $('input[name="tel"]').val();
         var text = $('input[name="text"]').val();
 
-        var msg = `●第一希望日：${date01}\n●第二希望日：${date02}\n●第三希望日：${date03}\n●人数：${number}\n●氏名：${names}\n●電話番号：${tel}\n●備考：${text}`;
+        var msg = `●第一希望日：${date01}\n∟希望時間：${time01}\n●第二希望日：${date02}\n∟希望時間：${time02}\n●第三希望日：${date03}\n∟希望時間：${time03}\n●人数：${number}\n●氏名：${names}\n●電話番号：${tel}\n●備考：${text}`;
         sendText(msg);
 
         return false;
