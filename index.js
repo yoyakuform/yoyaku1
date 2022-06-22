113713b07eb8b498861ebf231068febd90670851
yoyakuform <107911330+yoyakuform@users.noreply.github.com>
2022-06-22 10:54:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function () {
     // カレンダー
     $(function () {
-        $('input[name="date00"]').datepicker({
+        $('input[name="date01"]').datepicker({
             dateFormat: 'yy/mm/dd',
         });
     });
